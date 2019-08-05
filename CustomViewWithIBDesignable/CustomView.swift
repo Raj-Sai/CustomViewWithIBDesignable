b4f047ce6fc172bc0e50750d1521d983d564d390
RajSai <amsarajm.acumen@gmail.com>
2019-08-05 07:44:45 +0700

    Initial commit

---
+++ CustomViewWithIBDesignable/CustomView.swift
@@ -0,0 +1,76 @@
+//
+//  CustomView.swift
+//  CustomViewWithIBDesignable
+//
+//  Created by Amsaraj Mariappan on 2/8/2562 BE.
+//  Copyright © 2562 Amsaraj Mariyappan. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable open class CustomView: UIView {
+
+
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }
+        set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var shadowRadius: Double {
+        get {
+            return Double(self.layer.shadowRadius)
+        }
+        set {
+            self.layer.shadowRadius = CGFloat(newValue)
+        }
+    }
+}
---
+++ CustomViewWithIBDesignable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomViewWithIBDesignable
+//
+//  Created by Amsaraj Mariappan on 2/8/2562 BE.
+//  Copyright © 2562 Amsaraj Mariyappan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
